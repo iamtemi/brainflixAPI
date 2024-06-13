567e58a6aae7a6d4572ae290c6c2f95612167258
Temi Adenuga <a.adenuga@hotmail.com>
2024-06-13 13:05:42 +0100

    Create endpoint to get video by id

--- middleware/api-key.js
+++ middleware/api-key.js
@@ -6,16 +6,12 @@ const checkApiKey = (req, res, next) => {
   }
 
   if (!isNaN(apiKey) || !apiKey) {
-    console.log(typeof apiKey);
-
     return res.status(401).json({
       message:
         "api_key query parameter required. You may use any string (including your name) as your api_key.",
     });
   }
 
-  console.log(apiKey);
-  console.log(typeof apiKey);
   next();
 };
 
--- routes/videos.js
+++ routes/videos.js
@@ -3,12 +3,10 @@ import fs from "fs";
 
 const router = express.Router();
 
-
+const videosJson = fs.readFileSync("./data/video-details.json");
+const parsedVideos = JSON.parse(videosJson);
 
 router.get("/", (req, res) => {
-  const videosJson = fs.readFileSync("./data/video-details.json");
-  const parsedVideos = JSON.parse(videosJson);
-
   const filteredVideos = parsedVideos.map((video) => {
     const { id, title, channel, image } = video;
     return { id, title, channel, image };
@@ -16,5 +14,12 @@ router.get("/", (req, res) => {
 
   res.json(filteredVideos);
 });
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const videoDetail = parsedVideos.find((video) => id === video.id);
+  res.json(videoDetail);
+});
+
 
 export default router;

760f1d42f7ee9a06c84416dd5b5a06d326bb3731
Temi Adenuga <a.adenuga@hotmail.com>
2024-06-13 20:49:24 +0100

    create post videos endpoint

--- index.js
+++ index.js
@@ -8,18 +8,12 @@ import apiKeyMiddleWare from "./middleware/api-key.js";
 const app = express();
 app.use(express.json());
 
+app.use(express.static("./public"));
+
 app.use(apiKeyMiddleWare);
 
 const PORT = process.env.PORT || 8080;
 const BASE_URL = process.env.BASE_URL || localhost;
-
-app.get("/register", (req, res) => {
-  const apiKey = uuid();
-
-  res.json({
-    api_key: `${apiKey}`,
-  });
-});
 
 app.use("/videos", videosRouter);
 
--- routes/videos.js
+++ routes/videos.js
@@ -7,7 +7,9 @@ const router = express.Router();
 
 const videosJson = fs.readFileSync("./data/video-details.json");
 const parsedVideos = JSON.parse(videosJson);
+const time = new Date().getTime();
 
+// GET VIDEOS
 router.get("/", (req, res) => {
   const filteredVideos = parsedVideos.map((video) => {
     const { id, title, channel, image } = video;
@@ -17,12 +19,53 @@ router.get("/", (req, res) => {
   res.json(filteredVideos);
 });
 
+// GET VIDEO BY ID
 router.get("/:id", (req, res) => {
   const id = req.params.id;
+
   const videoDetail = parsedVideos.find((video) => id === video.id);
+
+  if (!videoDetail) {
+    return res.status(404).json({
+      message: "No video with that id exists",
+    });
+  }
+
   res.json(videoDetail);
 });
 
+// POST VIDEO
+router.post("/", (req, res) => {
+  const { title, description } = req.body;
+
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return res.status(400).json({
+      message: "Incomplete POST body",
+      requiredProperties: ["title", "description"],
+    });
+  }
+
+  const newVideo = {
+    id: uuid(),
+    title: title,
+    channel: "Aiden Thompson",
+    image: "../public/image1.jpg",
+    description: description,
+    views: 0,
+    likes: 0,
+    duration: "4:01",
+    video: "https://unit-3-project-api-0a5620414506.herokuapp.com/stream",
+    timestamp: time,
+    comments: [],
+  };
+
+  parsedVideos.push(newVideo);
+  fs.writeFileSync("./data/video-details.json", JSON.stringify(parsedVideos));
+
+  res.json(newVideo);
+});
+
+// POST COMMENT TO VIDEO
 router.post("/:id/comments", (req, res) => {
   const { name, comment } = req.body;
 
@@ -32,8 +75,6 @@ router.post("/:id/comments", (req, res) => {
       requiredProperties: ["comment", "name"],
     });
   }
-
-  const time = new Date().getTime();
 
   const newComment = {
     name: name,

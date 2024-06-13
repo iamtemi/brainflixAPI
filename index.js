950e1876cca99fe539cf4354492d8b9ebef0dba3
Temi Adenuga <a.adenuga@hotmail.com>
2024-06-13 21:00:25 +0100

    install cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import fs from "fs";
 import { v4 as uuid } from "uuid";
 import videosRouter from "./routes/videos.js";
 import apiKeyMiddleWare from "./middleware/api-key.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(express.static("./public"));
 
 app.use(apiKeyMiddleWare);
+
+app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 const BASE_URL = process.env.BASE_URL || localhost;
